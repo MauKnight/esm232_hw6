927f91aff07d697d14f865c0602d44d9de663bea
MauKnight <nmcolladoduran@gmail.com>
2021-05-10 11:19:30 -0700

    Renaming, final rmd and pdf

--- fgfx.R
+++ fgfx.R
@@ -6,16 +6,17 @@
 #' @parms parms$f - canopy closure threshold
 #' @parms parms$K - carrying capacity
 #' @return change in population 
-#'
+#' Authors: David Segan, Mauricio Collado, Simone Alburqueque and Siya Qiu
 forest_growth = function(time, C, parms) {
   
   # compute rate of change of forest
   dexfor = parms$r*C
   
-  # set rate of change to 0 if C is greater than K 
-  # else, set rate of change to g if C is greater than f
-  # else, default rate of change is r*C
   
-  dexfor =ifelse(C > parms$K, 0, ifelse(C > parms$f, parms$g, dexfor))
+  # establish the conditionals between carrying capacity and canopy closure
+  dexfor =ifelse(C > parms$K, 0, # set rate of change to 0 if C > K 
+                 ifelse(C > parms$f, parms$g, # else, set rate of change to g if C > f
+                        dexfor)) # else, default rate of change is r*C
+  
   return(list(dexfor))
 }
